4b33773d1c19e24fc855c1261ce7a79656a95b3d
craigomatic <craig@seventhcap.com>
2014-06-18 13:48:16 -0700

    Improved the loading and offline overlays for WP8

--- src/HybridWebApp.Toolkit.WP8/Views/LoadingOverlay.xaml.cs
+++ src/HybridWebApp.Toolkit.WP8/Views/LoadingOverlay.xaml.cs
@@ -7,14 +7,101 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
 
 namespace HybridWebApp.Toolkit.WP8.Views
 {
-    public partial class LoadingOverlay : UserControl
+    public partial class LoadingOverlay : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Brush _BackgroundBrush;
+
+        public Brush BackgroundBrush
+        {
+            get { return _BackgroundBrush; }
+            set
+            {
+                if (_BackgroundBrush == value)
+                {
+                    return;
+                }
+
+                _BackgroundBrush = value;
+                _OnPropertyChanged("BackgroundBrush");
+            }
+        }
+
+        private Brush _ForegroundBrush;
+
+        public Brush ForegroundBrush
+        {
+            get { return _ForegroundBrush; }
+            set
+            {
+                if (_ForegroundBrush == value)
+                {
+                    return;
+                }
+
+                _ForegroundBrush = value;
+                _OnPropertyChanged("ForegroundBrush");
+            }
+        }
+
+        private ImageSource _BackgroundImage;
+
+        public ImageSource BackgroundImage
+        {
+            get { return _BackgroundImage; }
+            set
+            {
+                if (_BackgroundImage == value)
+                {
+                    return;
+                }
+
+                _BackgroundImage = value;
+                _OnPropertyChanged("BackgroundImage");
+            }
+        }
+
+        private double _BackgroundImageWidth;
+
+        public double BackgroundImageWidth
+        {
+            get { return _BackgroundImageWidth; }
+            set
+            {
+                if(_BackgroundImageWidth == value)
+                {
+                    return;
+                }
+
+                _BackgroundImageWidth = value;
+                _OnPropertyChanged("BackgroundImageWidth");
+            }
+        }
+
         public LoadingOverlay()
         {
             InitializeComponent();
+
+            this.ForegroundBrush = Application.Current.Resources["PhoneAccentBrush"] as SolidColorBrush;
+            this.BackgroundBrush = Application.Current.Resources["PhoneBackgroundBrush"] as SolidColorBrush;
+            this.BackgroundImageWidth = 336;
+            
+            this.DataContext = this;
+        }
+
+        private void _OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
--- src/HybridWebApp.Toolkit.WP8/Views/OfflineOverlay.xaml.cs
+++ src/HybridWebApp.Toolkit.WP8/Views/OfflineOverlay.xaml.cs
@@ -7,21 +7,143 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using System.ComponentModel;
 
 namespace HybridWebApp.Toolkit.WP8.Views
 {
-    public partial class OfflineOverlay : UserControl
+    public partial class OfflineOverlay : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _LargeFontSize;
+
+        public int LargeFontSize
+        {
+            get { return _LargeFontSize; }
+            set
+            {
+                if (_LargeFontSize == value)
+                {
+                    return;
+                }
+
+                _LargeFontSize = value;
+                _OnPropertyChanged("LargeFontSize");
+            }
+        }
+
+        private int _SmallFontSize;
+
+        public int SmallFontSize
+        {
+            get { return _SmallFontSize; }
+            set
+            {
+                if (_SmallFontSize == value)
+                {
+                    return;
+                }
+
+                _SmallFontSize = value;
+                _OnPropertyChanged("SmallFontSize");
+            }
+        }
+
+        private Brush _ForegroundBrush;
+
+        public Brush ForegroundBrush
+        {
+            get { return _ForegroundBrush; }
+            set
+            {
+                if (_ForegroundBrush == value)
+                {
+                    return;
+                }
+
+                _ForegroundBrush = value;
+                _OnPropertyChanged("ForegroundBrush");
+            }
+        }
+
+        private Brush _BackgroundBrush;
+
+        public Brush BackgroundBrush
+        {
+            get { return _BackgroundBrush; }
+            set
+            {
+                if (_BackgroundBrush == value)
+                {
+                    return;
+                }
+
+                _BackgroundBrush = value;
+                _OnPropertyChanged("BackgroundBrush");
+            }
+        }
+
+        private ImageSource _BackgroundImage;
+
+        public ImageSource BackgroundImage
+        {
+            get { return _BackgroundImage; }
+            set
+            {
+                if (_BackgroundImage == value)
+                {
+                    return;
+                }
+
+                _BackgroundImage = value;
+                _OnPropertyChanged("BackgroundImage");
+            }
+        }
+
+        private double _BackgroundImageWidth;
+
+        public double BackgroundImageWidth
+        {
+            get { return _BackgroundImageWidth; }
+            set
+            {
+                if (_BackgroundImageWidth == value)
+                {
+                    return;
+                }
+
+                _BackgroundImageWidth = value;
+                _OnPropertyChanged("BackgroundImageWidth");
+            }
+        }
+
         public Action RetryAction { get; set; }
 
         public OfflineOverlay()
         {
             InitializeComponent();
+
+            this.ForegroundBrush = new SolidColorBrush(Colors.Black);
+            this.BackgroundBrush = new SolidColorBrush(Colors.White);
+            this.BackgroundImageWidth = 336;
+            this.LargeFontSize = 16;
+            this.SmallFontSize = 12;
+
+            this.DataContext = this;
         }
 
         private void Retry_Click(object sender, RoutedEventArgs e)
         {
             this.RetryAction();
+        }
+
+        private void _OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
