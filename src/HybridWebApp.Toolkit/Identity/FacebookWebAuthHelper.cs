e787936945b1abe657bdba33726874f610eb8d1c
Craig Presti <craig@seventhcap.com>
2014-09-12 11:05:18 -0700

    Added identity helpers to Toolkit project, enabled nuget package restore for solution.

---
+++ src/HybridWebApp.Toolkit/Identity/FacebookWebAuthHelper.cs
@@ -0,0 +1,40 @@
+﻿using HybridWebApp.Toolkit.Identity.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Web.Http;
+
+namespace HybridWebApp.Toolkit.Identity
+{
+    public class FacebookWebAuthHelper : WebAuthHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <param name="scopes"></param>
+        /// <param name="authenticationAction">First param is the request URI, second the callback URI</param>
+        public FacebookWebAuthHelper(string appId, string scopes, Action<Uri, Uri> authenticationAction)
+            :base(appId, scopes, authenticationAction)
+        {
+            this.Provider = "facebook";
+        }
+
+        public override void StartWebAuthentication()
+        {
+            var callbackUri = new Uri("https://www.facebook.com/connect/login_success.html");
+            var requestUri = new Uri(string.Format("https://m.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&response_type=token&scope={2}", _AppID, callbackUri.ToString(), _Scopes));
+
+            _AuthenticationAction(requestUri, callbackUri);
+        }
+
+        public override async Task<UserInfo> RetrieveForAccessToken(string accessToken)
+        {
+            return await _FetchUserInfo(new Uri(string.Format("https://graph.facebook.com/me?access_token={0}", accessToken)));
+        }
+    }
+}
---
+++ src/HybridWebApp.Toolkit/Identity/GoogleWebAuthHelper.cs
@@ -0,0 +1,43 @@
+﻿using HybridWebApp.Toolkit.Identity.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Web.Http;
+
+namespace HybridWebApp.Toolkit.Identity
+{
+    public class GoogleWebAuthHelper : WebAuthHelper
+    {
+        public GoogleWebAuthHelper(string appId, string scopes, Action<Uri, Uri> authenticationAction)
+            :base(appId, scopes, authenticationAction)
+        {
+            this.Provider = "google";
+        }
+
+        public override void StartWebAuthentication()
+        {
+            var callbackUri = new Uri("http://localhost");
+            var requestUri = new Uri(string.Format("https://accounts.google.com/o/oauth2/auth?client_id={0}&redirect_uri={1}&scope={2}&response_type=code", _AppID, callbackUri.ToString(), _Scopes));
+
+            _AuthenticationAction(requestUri, callbackUri);
+        }
+
+        public async override Task<Model.UserInfo> RetrieveForAccessToken(string accessToken)
+        {
+            //get the email, name, birthday, gender from the provider
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+
+                var response = await httpClient.GetAsync(new Uri("https://www.googleapis.com/plus/v1/people/me"));
+                var json = await response.Content.ReadAsStringAsync();
+
+                return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<UserInfo>(json));
+            }
+        }
+    }
+}
