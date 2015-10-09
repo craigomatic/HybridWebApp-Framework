d8cd2c4bd28cc23e2874392ab3b826668e5ddd24
Craig Presti <craig@seventhcap.com>
2015-10-09 08:45:15 -0700

    Upgrade the test project for W10

--- test/HybridWebApp.Framework.Test/Properties/AssemblyInfo.cs
+++ test/HybridWebApp.Framework.Test/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HybridWebApp.Framework.Test")]
+[assembly: AssemblyTitle("HybridWebApp.Framework.Test1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HybridWebApp.Framework.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("HybridWebApp.Framework.Test1")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform","UAP")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,3 +27,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
